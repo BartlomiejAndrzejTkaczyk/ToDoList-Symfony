907475a8598a1321a4cf5617c396520235b40b63
kruk97 <bartekandrzejtkaczyk@gmail.com>
2022-07-21 15:41:56 +0200

    a

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -59,13 +59,31 @@ class MainController extends AbstractController
         return $this->redirectToRoute('main_index');
     }
 
-    #[Route('/edit/{id}/{name}', name: 'main_edit_task')]
-    public function edit(int $id, string $name) : Response
+    #[Route("/edit/{id}", methods: ['POST'])]
+    public function editPost(int $id): Response
     {
-        $code = self::$dbAccess->editTask($id, $name);
-        if ($code == 200){
-            return $this->redirectToRoute('main_index');
-        }
-        return new Response(status: $code);
+        return new Response($_POST['newName'] . ' ' . $id);
     }
+
+    #[Route('/edit', methods: ['GET'])]
+    public function edit()
+    {
+        return $this->render(
+            'to-do-list/task-edit.html.twig',
+            [
+                'name' => 'Nic',
+                'id' => 1
+            ]
+        );
+    }
+
+//    #[Route('/edit/{id}/{name}', name: 'main_edit_task')]
+//    public function edit(int $id, string $name) : Response
+//    {
+//        $code = self::$dbAccess->editTask($id, $name);
+//        if ($code == 200){
+//            return $this->redirectToRoute('main_index');
+//        }
+//        return new Response(status: $code);
+//    }
 }
